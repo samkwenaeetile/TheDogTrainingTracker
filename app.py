aa2f82f78ad92f4c869198793dfaa98a965171e9
samantha kwenaeetile <skwenaeetile@icloud,com>
2026-06-28 01:54:24 +0100

    add navigation

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/add-dog")
+def add_dog():
+    return render_template("add_dog.html")
+
+
+@app.route("/view-dogs")
+def view_dogs():
+    return render_template("view_dogs.html")
+
+
+@app.route("/add-session")
+def add_session():
+    return render_template("add_session.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)

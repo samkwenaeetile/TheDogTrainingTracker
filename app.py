2ac31fe44a15e92037665d3081226f9b617b2f4d
samantha kwenaeetile <skwenaeetile@icloud,com>
2026-06-30 00:23:41 +0100

    Add the dog profile featuers and connect the dog profiles to sqlite

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+import sqlite3
 
 app = Flask(__name__)
 
@@ -15,13 +17,44 @@ def add_dog():
 
 @app.route("/view-dogs")
 def view_dogs():
-    return render_template("view_dogs.html")
+
+    connection = sqlite3.connect("database/dog_tracker.db")
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM dogs")
+
+    dogs = cursor.fetchall()
+
+    connection.close()
+
+    return render_template("view_dogs.html", dogs=dogs)
 
 
 @app.route("/add-session")
 def add_session():
     return render_template("add_session.html")
 
+@app.route("/save-dog", methods=["POST"])
+def save_dog():
+
+    dog_name = request.form["dog_name"]
+    breed = request.form["breed"]
+    age = request.form["age"]
+    owner_name = request.form["owner"]
+
+    connection = sqlite3.connect("database/dog_tracker.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        INSERT INTO dogs (dog_name, breed, age, owner_name)
+        VALUES (?, ?, ?, ?)
+    """, (dog_name, breed, age, owner_name))
+
+    connection.commit()
+    connection.close()
+
+    return redirect("/view-dogs")
 
 if __name__ == "__main__":
     app.run(debug=True)

276dd043d5463e74890fae835f5a9adda2dff0d0
samantha kwenaeetile <skwenaeetile@icloud,com>
2026-07-06 07:40:52 +0100

    Implement relational databse desgin for tranining sessions

--- create_database.py
+++ create_database.py
@@ -5,12 +5,15 @@ connection = sqlite3.connect("database/dog_tracker.db")
 cursor = connection.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS dogs (
-    dog_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    dog_name TEXT NOT NULL,
-    breed TEXT NOT NULL,
-    age INTEGER,
-    owner_name TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS training_sessions (
+    session_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dog_id INTEGER NOT NULL,
+    training_date TEXT NOT NULL,
+    training_type TEXT NOT NULL,
+    duration INTEGER,
+    notes TEXT,
+    status TEXT,
+    FOREIGN KEY (dog_id) REFERENCES dogs(dog_id)
 )
 """)
 
